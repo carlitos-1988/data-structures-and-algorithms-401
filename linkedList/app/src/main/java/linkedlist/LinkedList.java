931294e412a28a80cb4461dbf4290089414d8c30
Juan C Olmedo <doubleparked88@gmail.com>
2023-07-14 21:42:21 -0700

    finished logic for code needs tests generate

---
+++ linkedList/app/src/main/java/linkedlist/LinkedList.java
@@ -0,0 +1,63 @@
+package linkedlist;
+
+public class LinkedList {
+    Node head = null;
+    Node tail = null;  // not strictly required
+
+    public LinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+
+    //Will insert at the end of the linkedList seemed more challenging
+    public void insert(int value)
+    {
+        Node trackedNode = new Node(value);
+
+        if(this.head == null){
+            this.head = trackedNode;
+        }else{
+            Node currentNode = head;
+            while (currentNode.next != null){
+                currentNode = currentNode.next;
+            }
+            currentNode.setNext(trackedNode);
+        }
+
+
+
+    }
+    public boolean includes(int value)
+    {
+
+        Node curr = this.head;
+
+        while (curr.next != null){
+            if(curr.value == value){
+                return true;
+            }
+            curr = curr.next;
+        }
+
+        return false;
+    }
+
+
+    public StringBuilder toStringA()
+    {
+        StringBuilder toStringLinkedList = new StringBuilder();
+        Node curr = this.head;
+        char myCharTracker = 97;
+        while (curr != null){
+            toStringLinkedList.append(" { ");
+            toStringLinkedList.append(myCharTracker);
+            toStringLinkedList.append(" } -> ");
+
+            myCharTracker ++;
+            curr = curr.next;
+        }
+
+        return toStringLinkedList;
+    }
+}
---
+++ linkedList/app/src/main/java/linkedlist/Node.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+public class Node {
+    public int value;
+    public Node next = null;
+
+    Node(int value)
+    {
+        this.value = value;
+    }
+
+    public void setNext(Node next) {
+        if(next.equals(null)){
+            System.out.println("Empty Node passed In");
+        } else{
+            this.next = next;
+        }
+
+    }
+}
